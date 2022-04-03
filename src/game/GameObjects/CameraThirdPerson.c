86f6a131c10683ae97433fccedae5fe2928b745c
VoxAndrews <VoxAndrews@users.noreply.github.com>
2022-04-03 15:15:00 +0800

    Update CameraThirdPerson.c
    Still trying to figure out how to set the 'playerObject' variable as it is causing memory access issues

--- src/game/GameObjects/CameraThirdPerson.c
+++ src/game/GameObjects/CameraThirdPerson.c
@@ -29,6 +29,8 @@ Vector3 camUp; //The camera's Up-Vector
 OnStart OnCamStart(GameObject* gameObject) //Sets the starting variables of the third person camera object
 {
 	gameObject->name = "player camera"; //Sets object name
+
+	gameObject->transform.position = (Vector3){ 0.0f, 0.0f, 0.0f }; //Sets the camera's initial position
 	
 	playerObject = GameObjectManagerFind(&gameManager, 2); //Sets the Player Object
 
@@ -47,16 +49,20 @@ OnUpdate OnCamUpdate(Time time, GameObject* gameObject) //Calculates the positio
 	Vector3 focusPoint = playerObject->transform.position; //The point that the camera is focusing on
 	Vector3 lookAtDir = { camForwardDir.x, camForwardDir.y, camForwardDir.z }; //The direction (Forward vector), the camera is looking at
 
-	camPos.x = focusPoint.x - lookAtDir.x * camDistance; //Calculates the camera's new x position
-	camPos.y = focusPoint.y - lookAtDir.y * camDistance; //Calculates the camera's new y position
-	camPos.z = focusPoint.z - lookAtDir.z * camDistance; //Calculates the camera's new z position
+	gameObject->transform.position.x = focusPoint.x - lookAtDir.x * camDistance; //Calculates the camera's new x position
+	gameObject->transform.position.y = focusPoint.y - lookAtDir.y * camDistance; //Calculates the camera's new y position
+	gameObject->transform.position.z = focusPoint.z - lookAtDir.z * camDistance; //Calculates the camera's new z position
+
+	camPos.x = gameObject->transform.position.x; //Sets the variable camera position variable (used in gluLookAt), to the x position of camera object
+	camPos.y = gameObject->transform.position.y; //Sets the variable camera position variable (used in gluLookAt), to the y position of camera object
+	camPos.z = gameObject->transform.position.z; //Sets the variable camera position variable (used in gluLookAt), to the z position of camera object
 
 	//CAMERA POSITION CHANGE
 
 	//Changes the position/rotation of the camera
-	gluLookAt(	camPos.x, camPos.y, camPos.z,				//Camera X, Y, Z Position
-				focusPoint.x, focusPoint.y, focusPoint.z,	//Camera X, Y, Z Focal Point
-				camUp.x, camUp.y, camUp.z);					//Camera X, Y, Z Up-Vector
+	gluLookAt(	camPos.x, camPos.y, camPos.z,					//Camera X, Y, Z Position
+				focusPoint.x, focusPoint.y, focusPoint.z,		//Camera X, Y, Z Focal Point
+				camUp.x, camUp.y, camUp.z);						//Camera X, Y, Z Up-Vector
 }
 
 //////////////////////////////
