0176763c160bba7d8276fb1219c2a70f4fee8b33
Wolfleader <james.morgan@iinet.net.au>
2022-05-22 18:16:17 +0800

    physics in seperate file

--- src/game/Game.h
+++ src/game/Game.h
@@ -4,10 +4,10 @@
 #include "game/GameIncludes.h"
 #include "game/GameTypes.h"
 #include "game/GameConstants.h"
+#include "game/Physics.h"
 
 
 void UpdateGameObjects(Time time, GameObjectManager* gameObjectManager);
-void FixedUpdateGameObjects(Time fixedTime, GameObjectManager* gameObjectManager);
 
 void InitGameObject(GameObject* gameObject); // initialise the game object
 void SetupCallbacks(GameObject* gameObject, OnStart OnStart, OnUpdate OnUpdate, OnLateUpdate OnLateUpdate, OnFixedUpdate OnFixedUpdate); // setup the callbacks
@@ -15,16 +15,10 @@ void FreeGameObject(GameObject* gameObject); // free the game object
 
 void InitTransform(Transform* transform);
 void InitMesh(Mesh* mesh);
-void InitRigidBody(RigidBody* RigidBody);
 
 void UpdateGameObject(Time time, GameObject* gameObject); // called every frame update
-void FixedUpdateGameObject(Time fixedTime, GameObjectManager* gameObjectManager, GameObject* gameObject); // caled for physics update
-void GravityTransform(Time fixedTime, GameObject* gameObject);
 
 void UpdateTransform(Time time, Transform* transform);
 void UpdateMesh(Time time, Mesh* mesh);
 void DrawGizmos(Time time, Vector3 maxSize);
 void DrawBoundingBox(BoudingBox box);
-
-void CalculateBoundingBox(GameObject* gameObject);
-void DetectCollision(Time fixedTime, GameObjectManager* gameObjectManager, GameObject* gameObject);
--- src/game/Physics.h
+++ src/game/Physics.h
@@ -2,3 +2,11 @@
 
 #include "game/GameIncludes.h"
 #include "game/GameTypes.h"
+#include "game/GameConstants.h"
+
+void InitRigidBody(RigidBody* RigidBody);
+void FixedUpdateGameObjects(Time fixedTime, GameObjectManager* gameObjectManager);
+void FixedUpdateGameObject(Time fixedTime, GameObjectManager* gameObjectManager, GameObject* gameObject); // caled for physics update
+void GravityTransform(Time fixedTime, GameObject* gameObject);
+void CalculateBoundingBox(GameObject* gameObject);
+void DetectCollision(Time fixedTime, GameObjectManager* gameObjectManager, GameObject* gameObject);
